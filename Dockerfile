2726112d24dcdd370b2baf8b96144e33903b3619
mai1015 <mai1015@users.noreply.github.com>
2020-05-21 20:49:29 -0400

    add config

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM jboss/keycloak:10.0.1
 
 ADD ./modules /opt/jboss/keycloak/modules
 ADD ./tools /opt/jboss/tools
+ADD ./startup-scripts /opt/jboss/startup-scripts
 
 RUN /bin/sh /opt/jboss/tools/module.sh
--- tools/module.sh
+++ tools/module.sh
@@ -6,4 +6,4 @@ bin/jboss-cli.sh --file=/opt/jboss/tools/cli/module/standalone-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/module/standalone-ha-configuration.cli
-rm -rf standalone/configuration/standalone_xml_history/current/*
+rm -rf standalone/configuration/standalone_xml_history
